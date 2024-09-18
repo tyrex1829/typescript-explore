580c8afe147170a5fa7e7ccfedf2729a346ce2f6
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-18 11:55:02 +0530

    types to function

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
 "use strict";
 const x = 1;
 console.log(x);
+function great(x) {
+    console.log("Hello " + x);
+}
+great("Saksham");
--- app.ts
+++ app.ts
@@ -1,2 +1,8 @@
 const x: number = 1;
 console.log(x);
+
+function great(x: String) {
+  console.log("Hello " + x);
+}
+
+great("Saksham");
