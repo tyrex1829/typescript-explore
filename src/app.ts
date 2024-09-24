e7ceb7d2a32a4fc27e273dc38f267270bc021d1b
tyrex1829 <sakshamkhare19@gmail.com>
2024-09-24 14:15:28 +0530

    generics

--- src/app.ts
+++ src/app.ts
@@ -163,3 +163,13 @@ doSomething(Direction.Up);
 doSomething(Direction.Down);
 console.log(Direction.Down);
 console.log(Direction.Up);
+
+// Generics
+
+function args<T>(arg: T): T {
+  return arg;
+}
+
+const out1 = args<string>("Saksham");
+const out2 = args<number>(1000);
+console.log(out1 + " " + out2);
